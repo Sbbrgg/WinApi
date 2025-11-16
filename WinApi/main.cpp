bf5db33e20d9610b2cd85f99a1cbf2e6a6d25d11
un <un@DESKTOP-JAU235M>
2025-11-16 23:03:51 +0300

    Copy DONE TODO: PlaceHolder

--- WinApi/main.cpp
+++ WinApi/main.cpp
@@ -10,17 +10,17 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL,
 		"Hello WinAPI!",
 		"MessageBox",
-		MB_YESNOCANCEL 
-		| MB_ICONERROR 
-		| MB_HELP 
-		| MB_DEFBUTTON3 
+		MB_YESNOCANCEL
+		| MB_ICONERROR
+		| MB_HELP
+		| MB_DEFBUTTON3
 		| MB_SYSTEMMODAL
 		| MB_RIGHT
 	);*/
 	//MB - message box
 
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DLGPROC(DlgProg), 0);
-	
+
 	//MB_ICONINFORMATION
 	// Information, question, error, warning
 	return 0;
@@ -30,13 +30,34 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:	//Выполняется один раз, при запуска окна
-		break;
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
+	}
+	break;
 	case WM_COMMAND:	//Обрабатывает команды с клавиатуры и мыши.
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+
+
+			
+		}
+		break;
 		case IDOK:
 			MessageBox(NULL, "Была нажата кнопка 'OK'", "Info", MB_OK | MB_ICONINFORMATION);
-				break;
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
 		}
 		break;
 	case WM_CLOSE:		//Выполняется при нажатии кнопки "X".
