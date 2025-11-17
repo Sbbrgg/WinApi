be1b30eaf3b18e617b23bd7e606a1398745ecdd5
un <un@DESKTOP-JAU235M>
2025-11-17 21:42:46 +0300

    ComboBox start

--- WinApi/main.cpp
+++ WinApi/main.cpp
@@ -2,9 +2,9 @@
 #include<CommCtrl.h>
 #include"resource.h"
 
+CONST CHAR g_sz_INVITE[] = "¬ведите логин: ";
+
 BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-CONST CHAR TextForLogin[] = "¬ведите логин: ";
-CONST CHAR TextForPassword[] = "¬ведите пароль: ";
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	/*MessageBox
@@ -36,13 +36,28 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
-		SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), EM_SETCUEBANNER, FALSE, (LPARAM)L"¬ведите логин: ");
-		SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), EM_SETCUEBANNER, FALSE, (LPARAM)L"¬ведите пароль: ");
+		//SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), EM_SETCUEBANNER, FALSE, (LPARAM)L"¬ведите логин: ");
+		//SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), EM_SETCUEBANNER, FALSE, (LPARAM)L"¬ведите пароль: ");
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
 	}
 	break;
 	case WM_COMMAND:	//ќбрабатывает команды с клавиатуры и мыши.
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = "";
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, g_sz_INVITE) == 0)
+			{
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			}
+			if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(sz_buffer, "") == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
+		}
+		break;
 		case IDC_BUTTON_COPY:
 		{
 			CONST INT SIZE = 256;
