1f4bbfa855112500548addfdbe50584544fcf46e
un <un@DESKTOP-JAU235M>
2025-11-19 19:43:41 +0300

    ComboBox DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,5 +1,6 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
 #include"resource1.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "fisrt", "Combo", "Box" };
@@ -31,7 +32,40 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 		case IDOK:
 		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};	//sz_ - String Zero (NULL Terminated Line) Обычная сишная строка
+			CHAR sz_message[SIZE]{};
 			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);	//GetCurrentSelection - получить текущий выбранный элемент
+			if (i != CB_ERR)
+			{
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+				sprintf(sz_message, "Вы выбрали элемент №%i со значением \"%s\".", i, sz_buffer);	//1.буффер куда помещаем сообщение
+				MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			else
+			{
+				MessageBox(hwnd, "Вы ничего не выбрали", "Info", MB_OK | MB_ICONWARNING);
+			}
+		}
+		break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+	}
+	break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
+
+
+
+//IDOK ВЫВОД ВЫБРАННОЙ ИНФОРМАЦИИ В COMBOBOX
+/*HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
 			int index = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
 			if (index != CB_ERR)
 			{
@@ -54,18 +88,4 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			else
 			{
 				MessageBox(hwnd, "Пожалуйста, выберите элемент из списка", "Предупреждение", MB_OK | MB_ICONWARNING);
-			}
-		}
-		break;
-		case IDCANCEL:
-			EndDialog(hwnd, 0);
-			break;
-		}
-	}
-	break;
-	case WM_CLOSE:
-		EndDialog(hwnd, 0);
-		break;
-	}
-	return FALSE;
-}
+			}*/
