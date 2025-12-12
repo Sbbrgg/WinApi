847698e09f6231cf65ddc5c33939db4345f45cb8
un <un@DESKTOP-JAU235M>
2025-12-12 21:45:14 +0300

    Code structure DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -2,45 +2,8 @@
 #include <Windows.h>
 #include <iostream>
 #include "resource.h"
-
-#define BACKGROUND_SQUARE_BLUE		RGB(132,165,240)
-#define BACKGROUND_METAL_MISTRAL	RGB(196,166,135)
-
-CONST CHAR g_sz_WINDOW_CLASS[] = "Calc PV_521";
-
-CONST INT g_i_BUTTON_SIZE = 60;
-CONST INT g_i_INTERVAL = 5;
-CONST INT g_i_DISPLAY_INTERVAL = 10;
-CONST INT g_i_DOUBLE_BUTTON_SIZE = g_i_BUTTON_SIZE * 2 + g_i_INTERVAL;
-CONST INT g_i_DISPLAY_WIDTH = g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4;
-CONST INT g_i_DISPLAY_HEIGHT = g_i_BUTTON_SIZE;
-CONST INT g_i_FONT_HEIGHT = g_i_DISPLAY_HEIGHT - 2;
-CONST INT g_i_FONT_WIDTH = g_i_FONT_HEIGHT / 2.5;
-CONST INT g_i_START_X = 10;
-CONST INT g_i_START_Y = 10;
-CONST INT g_i_BUTTON_START_X = g_i_START_X;
-CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_DISPLAY_INTERVAL;
-
-CONST INT g_i_WINDOW_WIDTH = g_i_DISPLAY_WIDTH + g_i_START_X * 2 + 16;
-CONST INT g_i_WINDOW_HEIGHT = g_i_DISPLAY_HEIGHT + g_i_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * 4 + 48 + g_i_DISPLAY_INTERVAL;
-
-HBRUSH g_hbrBackground = NULL;
-CHAR g_szCurrentSkin[32] = "square_blue"; // Текущая тема
-
-#define X_BUTTON_POSITION(position) g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) * position
-#define Y_BUTTON_POSITION(position) g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * position
-
-CONST CHAR g_OPERATIONS[] = "+-*/";
-
-CONST INT g_i_WINDOW_COLOR = 0;
-CONST INT g_i_DISPLAY_COLOR = 1;
-CONST INT g_i_FONT_COLOR = 2;
-CONST COLORREF g_clr_COLORS[][3] =
-{
-	{ RGB(0, 0, 150,), RGB(0, 0, 100), RGB(150, 0 , 0)},
-	{ RGB(150, 150, 150), RGB(50, 50, 50), RGB(0, 255, 0)},
-};
-CONST CHAR* g_sz_SKIN[] = {"square_blue", "metal_mistral"};
+#include "dimensions.h"
+#include "ColorsAndSkins.h"
 
 LRESULT CALLBACK WndProc(HWND hwnd, INT uMsg, WPARAM wParam, LPARAM lParam);
 VOID UpdateBackgroundBrush(CONST CHAR* skin);
